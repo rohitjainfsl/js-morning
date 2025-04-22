fb0a43d44d9c1fc5eb6bc08dc8e3df3153f6d4c6
rohitjainfsl <rohit@fullstacklearning.com>
2025-04-22 10:21:03 +0530

    22-04-2025

---
+++ javascript/variables.js
@@ -0,0 +1,42 @@
+//variables are containers for storing information / data
+
+// int a = 1;
+// float b=  1.5;
+// char c=  ["a", "b", "c"]
+// int a = "Rohit";
+
+// DIFFERENCE BETWEEN variable IN C & variable IN JS??
+// In C, we write data type before the variable name. In JS, we write a keyword before the variable name.
+// For example, in C, if we write "int a", then a can only have integer values. It cannot have values of any other data type.
+// In JS, since we do not write data type before variable name, we can put any value in the variable.
+
+// var a = "Swati";
+// var b = "Javascript";
+
+// document.writeln(b);
+
+// Swati is studying Javascript
+
+// CONCATENATION: combining variables & strings together using "+" sign
+// document.writeln(a + " is studying " + b);
+
+// var a = "Swati";
+// var b = "Kulsheshtra";
+//Hello, My name is Swati Kuls heshtra
+
+// document.writeln("hello, my name " + a + " " + b);
+
+// var n1 = 10;
+// var n2 = 20;
+// var n3 = 30;
+
+// //The Sum is 60
+
+// document.writeln("The sum is " + (n1 + n2 + n3));
+
+var x = "Harendra";
+var y = "Ketan";
+
+//Harendra and Ketan are friends
+
+document.writeln(x + " and " + y + " " + "are friends");
